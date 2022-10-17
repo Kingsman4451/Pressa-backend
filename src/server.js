6ab861f7a154974a13af5b852d51e6bbe8acc5a7
Kingsman4451 <dadosh4451@gmail.com>
2022-10-18 02:48:12 +0500

    fix

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import modules from './modules/index.js'
 
 const app = express();
 app.use(cors())
+app.use(express.static(path.join(process.cwd(), 'uploads')));
 app.use(express.json())
 app.use(modules)
 
